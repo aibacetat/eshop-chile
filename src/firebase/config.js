48990c9d750dd123ed7bc9653de23010437fa0c6
aibacetat <aaron.ibaceta@hotmail.com>
2023-04-12 19:02:35 -0400

    commit 1 firebase

---
+++ src/firebase/config.js
@@ -0,0 +1,30 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+
+// Your web app's Firebase configuration
+export const firebaseConfig = {
+
+  apiKey: "AIzaSyAnedyVpAkdFOm7fyiJOilNfxjYkgQidEY",
+
+  authDomain: "eshop-chile.firebaseapp.com",
+
+  projectId: "eshop-chile",
+
+  storageBucket: "eshop-chile.appspot.com",
+
+  messagingSenderId: "720565066587",
+
+  appId: "1:720565066587:web:d3158961964bcc3f9b587e"
+
+};
+
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const storage = getStorage(app);
+
+export default app;
